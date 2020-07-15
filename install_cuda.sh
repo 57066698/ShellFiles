cd9f02b77284274395f0be9c8e613390d34c1bda
root <root@localhost.localdomain>
2020-07-15 03:24:05 -0700

    111

--- install_cuda.sh
+++ install_cuda.sh
@@ -1,8 +1,7 @@
 yum install wget
 wget -O cuda.run http://developer.download.nvidia.com/compute/cuda/11.0.2/local_installers/cuda_11.0.2_450.51.05_linux.run
 bash cuda.run
-echo "export PATH=/usr/local/cuda/bin:'$'PATH" >> ~/.bashrc
-echo "export LD_LIBRARY_PATH=/usr/local/cuda/lib64:$LD_LIBRARY_PATH" >> ~/.bashrc
-
-# $vi ~/.bashrc , go bottom, replace '$' to $ 
-# $source ./bashrc
+cat <<EOT>> ~/.bashrc
+export PATH=/usr/local/cuda/bin:$PATH
+export LD_LIBRARY_PATH=/usr/local/cuda/lib64:$LD_LIBRARY_PATH
+EOT
