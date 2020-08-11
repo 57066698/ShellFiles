64d840b5d68e621815c64e3fdd67e76ec7aec9f5
root <root@localhost.localdomain>
2020-08-11 05:05:31 -0400

    111

---
+++ download.sh
@@ -0,0 +1,8 @@
+nvidia_driver=450.57
+cuda=10.2.89_440.33.01_linux.run
+cudnn=7
+
+echo "this is a script to download nvidia-driver, cuda, cudnn and anaconda for centos7 install"
+echo "it will download nvidia-driver, cuda, anaconda directly and then download cudnn from google cloud"
+echo "nvidia_driver=${nvidia_driver}, cuda=${cuda}, cudnn=&{cudnn}"
+
