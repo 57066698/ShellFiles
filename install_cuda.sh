8b3c9383fb28420f730616e8028349f82d719a29
root <root@localhost.localdomain>
2020-10-21 07:11:34 -0400

    0.1

--- install_cuda.sh
+++
@@ -1,7 +0,0 @@
-yum install wget
-wget -O cuda.run http://developer.download.nvidia.com/compute/cuda/11.0.2/local_installers/cuda_11.0.2_450.51.05_linux.run
-bash cuda.run
-cat <<EOT>> ~/.bashrc
-export PATH=/usr/local/cuda/bin:$PATH
-export LD_LIBRARY_PATH=/usr/local/cuda/lib64:$LD_LIBRARY_PATH
-EOT
--- install_cudnn.sh
+++
@@ -1,5 +0,0 @@
-# download cudnn tgz file and rename it to cudnn.tgz
-tar -xzvf cudnn.tgz
-sudo cp cuda/include/cudnn*.h /usr/local/cuda/include
-sudo cp cuda/lib64/libcudnn* /usr/local/cuda/lib64
-sudo chmod a+r /usr/local/cuda/include/cudnn*.h /usr/local/cuda/lib64/libcudnn*
